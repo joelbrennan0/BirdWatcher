7445e3f665da36a422a2ee87ba1336fdcddf5588
Joel Brennan <joel.brennan@tpagency.com>
2016-12-16 10:54:47 +0000

    Birdwatcher with example search feature

--- BirdWatcher/Program.cs
+++ BirdWatcher/Program.cs
@@ -10,6 +10,27 @@ namespace BirdWatcher
     {
         static void Main(string[] args)
         {
+            var searchParameters = new BirdSearch
+            {
+                Size = "Medium",
+                Country = "United States",
+                Colors = new List<string> { "White", "Brown", "Black" },
+                Page = 0,
+                PageSize = 5
+            };
+            Console.WriteLine("Type any key to begin search");
+            var birds = BirdRepository.LoadBirds();
+
+            while(Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine($"Page: {searchParameters.Page}");
+
+                birds.Search(searchParameters).ToList().ForEach(b =>
+                {
+                    Console.WriteLine($"Common Name: {b.CommonName}");
+                });
+                searchParameters.Page++;
+            }
         }
     }
 }
